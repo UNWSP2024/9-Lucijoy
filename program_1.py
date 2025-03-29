cf4cf27c7717104885709064fa88614db640611d
Lucijoy <ljfloan2@students.unwsp.edu>
2025-03-28 22:56:52 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,3 +1,6 @@
+# Author: Lucia Floan
+# Date: Mar 28, 2025
+# Title: Item Counter
 # Program #1: Item Counter
 # Assume a file containing a series of names (as strings) is named names.txt 
 # (Use the included example file names.txt) and exists on the computer's disk.
@@ -5,7 +8,23 @@
 
 def count_file_lines():
     ######################
-    # Add your code here #
+    try:
+        with open('names.txt', 'r') as file:
+            lines = file.readlines()
+
+        if lines:
+            print (f'{len(lines)} names were found')
+            print ('the names are:')
+            for name in lines:
+                print(f'- {name}')
+        else:
+            print('file is empty.')
+
+    except FileNotFoundError:
+        print("the file 'names.txt' does not seem to exist.")
+    except Exception as e:
+        print(f'something went wrong: {e}')
+    
     ######################
     print('In the count_file_lines function')
 
